3fbef99148c8e5987ad7c42b1f95180e0c2ef9e2
subodh-1407 <subodhgangwar0123@gmail.com>
2024-11-15 15:15:04 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import styles from './Header.module.css';  // Import the CSS Module
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      Currency Converter
+    </header>
+  );
+}
+
+export default Header;
