6fcc313868e2b4de8dc3a3b7cb5bfdfb2cb1850a
Subodh Gangwar <subodhgangwar0123@gmail.com>
2025-06-28 09:55:55 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,7 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.abc}>
-        <div className={styles.headerText}>Currency Converter</div>
-      </div>
-      
+      <div className={styles.headerText}>Currency Converter</div>
     </header>
   );
 }
